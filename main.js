5a8c961a08605c8e264493798b83cdd019babc6f
bachidze <bachidze90@gmail.com>
2023-11-24 18:45:34 +0400

    jsjs

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const newyeartime = new Date(`January 1 ${currentyear + 1} 00:00:00`);
 function updatecount() {
     const currenttime = new Date();
     const diff = newyeartime - currenttime;
-    const d = Math.floor(diff / 1000 / 73 / 60 / 24);
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
     const h = Math.floor(diff / 1000 / 60 / 60) % 24;
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
